1d5f67dc883a8cbd6c43762f47a926f1edd26ea7
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2020-02-10 15:19:47 +0000

    scripts: setting the bench script to work with bundled flamegraph analyzer

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 sudo perf record -F 997 -a -g --  target/release/sphinx-bench
 
-sudo perf script | ~/workspace/misc/FlameGraph/stackcollapse-perf.pl | ~/workspace/misc/FlameGraph/flamegraph.pl > ~/Desktop/pretty-graph.svg
+sudo perf script | flamegraph/stackcollapse-perf.pl | flamegraph/flamegraph.pl > output/pretty-graph.svg
